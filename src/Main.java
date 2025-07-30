7f0fcca4cf3e1a59ac197d710baff21c0c5b4419
Prakhyath Shri <prakhyath2004@gmail.com>
2025-07-30 21:33:42 +0530

    Stopping oops

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Pen {
+    String color;
+    String type;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public void write(){
+        System.out.println("Writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+}
+
+class Student {
+     String name;
+     int age;
+
+     public void printInfo(){
+         System.out.println(this.name);
+         System.out.println(this.age);
+     }
+
+     Student(String name, int age){
+         this.name = name;
+         this.age = age;
+     }
+
+     Student(Student s2){
+         this.name = s2.name;
+         this.age = s2.age;
+     }
+
+     Student(){
+         //Empty constructor
+     }
+}
+
+public class Main{
+    public static void main(String args[]){
+
+        Student s1 = new Student("Aman",25);
+        s1.name = "Yuva";
+        s1.age = 23;
+        s1.printInfo();
+
+        Student s2 = new Student(s1);
+        s2.printInfo();
+
+        Pen pen1 = new Pen();
+        pen1.color = "red";
+        pen1.type = "gel";
+        pen1.write();
+        pen1.printColor();
+
+        Pen pen2 = new Pen();
+        pen2.color = "White";
+        pen2.type = "Ball";
+        pen2.write();
+        pen2.printColor();
+
     }
 }
